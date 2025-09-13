b367ee4c7c5d3a75c3f81b958274abaa2c50598e
MiguelLariz <miguelangelduronlariz@gmail.com>
2025-09-13 00:45:49 -0600

    Parcial01 - Initial configuration

--- API/Program.cs
+++ API/Program.cs
@@ -1,23 +1,18 @@
+// Crera servicio web
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+// Agregar controladores
 builder.Services.AddControllers();
+
+// Crea el documento de la API, se puede eliminar para aligerar la API
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
