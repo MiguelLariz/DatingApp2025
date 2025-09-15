f3cf4b363cc8c8582c1a2d3df2f432b68df67bdb
MiguelLariz <miguelangelduronlariz@gmail.com>
2025-09-14 23:12:35 -0600

    Parcial01 - Ad CORS policy to API

--- API/Program.cs
+++ API/Program.cs
@@ -13,6 +13,9 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("SqliteConnection"));
 });
 
+// Poder consultar si tiene permisos
+builder.Services.AddCors();
+
 
 // Crea el documento de la API, se puede eliminar para aligerar la API
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
@@ -21,6 +24,15 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
+// Aceptar los permisos (seguridad) para poder consultarlo
+app.UseCors(x => x.AllowAnyHeader()
+    .AllowAnyMethod()
+    .WithOrigins(
+        "http://localhost:4200",
+        "https://localhost:4200"
+    ));
+
 app.MapControllers();
 
 app.Run();
