147df13627e182fd76ab25b1929236069fe6d6f7
MiguelLariz <miguelangelduronlariz@gmail.com>
2025-09-15 00:13:48 -0600

    Parcial01 - Display fetched users

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,11 +1,11 @@
 import { HttpBackend, HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Subscriber } from 'rxjs';
+import { lastValueFrom, Subscriber } from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
@@ -13,6 +13,9 @@ export class App implements OnInit {
 
   private http = inject(HttpClient);
   protected readonly title = signal("Dating App");
+
+  // Se cargan los elementos cada vez que se refresca la página
+  protected members = signal<any>([]);
 
   // Otra forma de llamarlo desde el html
   // quedaria de la siguiente forma:
@@ -23,13 +26,18 @@ export class App implements OnInit {
   // con el de inject
   // constructor(private http2: HttpClient) { }
 
-  ngOnInit(): void {
-    this.http.get("https://localhost:5001/api/members").subscribe({
-      next: response => console.log(response),
-      error: error => console.log(error),
-      complete: () =>console.log('Completed the http reqest')
-
-    });
+  async ngOnInit(): Promise<void> {
+    this.members.set(await this.getMembers());
   }
+
+  async getMembers(): Promise<object> {
+    try {
+      return await lastValueFrom(this.http.get("https://localhost:5001/api/members"))
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  }
+
 }
 
