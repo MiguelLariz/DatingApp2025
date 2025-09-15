3421bf127119cb7b0af9bf3139d98293a5b61fe2
MiguelLariz <miguelangelduronlariz@gmail.com>
2025-09-14 22:58:31 -0600

    Parcial01 - Add Http Client to SPA

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,5 +1,7 @@
-import { Component, signal } from '@angular/core';
+import { HttpBackend, HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Subscriber } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,7 +9,27 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('client');
-  protected title2 = "Hola";
+export class App implements OnInit {
+
+  private http = inject(HttpClient);
+  protected readonly title = signal("Dating App");
+
+  // Otra forma de llamarlo desde el html
+  // quedaria de la siguiente forma:
+  // <h1>{{title2}}</h1>
+  // protected title2 = "Hola";
+
+  // Constructor tradicional que se puede susituir
+  // con el de inject
+  // constructor(private http2: HttpClient) { }
+
+  ngOnInit(): void {
+    this.http.get("https://localhost:5001/api/members").subscribe({
+      next: response => console.log(response),
+      error: error => console.log(error),
+      complete: () =>console.log('Completed the http reqest')
+
+    });
+  }
 }
+
