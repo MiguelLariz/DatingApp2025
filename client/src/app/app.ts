55047aa3669af4a616a45b3b56573253c9921e0f
MiguelLariz <118703138+MiguelLariz@users.noreply.github.com>
2025-10-18 21:20:52 -0600

    Parcial01 - Add API and client skeletons (#1)
    * Parcial01 - Initial commit
    
    * Parcial01 - Initial configuration
    
    * Parcial01 - Add first entity
    
    * Parcial01 - Install EF Core for SQLite
    
    * Parcial01 - Add first EF migration
    
    * Parcial01 - Create database
    
    * Parcial01 - Add Members Controller
    
    * Parcial01 - Add Async code in Memebers Controller
    
    * Parcial01 - Add Angular skeleton
    
    * Parcial01 - Add Http Client to SPA
    
    * Parcial01 - Ad CORS policy to API
    
    * Parcial01 - Display fetched users
    
    * Parcial01 - Add Tailwind and DaisyUI for styling
    
    * Parcial01 - Add initial styling
    
    * Parcial01 - Add SSL
    
    * Parcial01 - Update readme

---
+++ client/src/app/app.ts
@@ -0,0 +1,43 @@
+import { HttpBackend, HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { lastValueFrom, Subscriber } from 'rxjs';
+
+@Component({
+  selector: 'app-root',
+  imports: [],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App implements OnInit {
+
+  private http = inject(HttpClient);
+  protected readonly title = signal("Dating App");
+
+  // Se cargan los elementos cada vez que se refresca la página
+  protected members = signal<any>([]);
+
+  // Otra forma de llamarlo desde el html
+  // quedaria de la siguiente forma:
+  // <h1>{{title2}}</h1>
+  // protected title2 = "Hola";
+
+  // Constructor tradicional que se puede susituir
+  // con el de inject
+  // constructor(private http2: HttpClient) { }
+
+  async ngOnInit(): Promise<void> {
+    this.members.set(await this.getMembers());
+  }
+
+  async getMembers(): Promise<object> {
+    try {
+      return await lastValueFrom(this.http.get("https://localhost:5001/api/members"))
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  }
+
+}
+
