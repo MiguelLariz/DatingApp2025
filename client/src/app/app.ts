c6c78d1e80be9b9238778d41a223d83cfe9f8857
MiguelLariz <miguelangelduronlariz@gmail.com>
2025-10-27 16:02:16 -0600

    Parcial02 - Add Parent to Child communication

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -5,6 +5,7 @@ import { lastValueFrom, Subscriber } from 'rxjs';
 import { Nav } from "../layout/nav/nav";
 import { AccountService } from '../core/services/account-service';
 import { Home } from "../features/home/home";
+import { User } from '../types/user';
 
 @Component({
   selector: 'app-root',
@@ -20,7 +21,7 @@ export class App implements OnInit {
   protected readonly title = signal("Dating App");
 
   // Se cargan los elementos cada vez que se refresca la página
-  protected members = signal<any>([]);
+  protected members = signal<User[]>([]);
 
   // Otra forma de llamarlo desde el html
   // quedaria de la siguiente forma:
@@ -43,9 +44,9 @@ export class App implements OnInit {
     this.accountService.currentUser.set(user);
   }
 
-  async getMembers(): Promise<object> {
+  async getMembers(): Promise<User[]> {
     try {
-      return await lastValueFrom(this.http.get("https://localhost:5001/api/members"))
+      return await lastValueFrom(this.http.get<User[]>("https://localhost:5001/api/members"))
     } catch (error) {
       console.log(error);
       throw error;
