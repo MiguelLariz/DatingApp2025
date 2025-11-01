d2db89a967eee16bc4e92f7b74d01269a4de1b41
MiguelLariz <miguelangelduronlariz@gmail.com>
2025-10-31 20:17:24 -0600

    Parcial02 - Implement member repo in controller

--- API/Controllers/MembersContrllers.cs
+++ API/Controllers/MembersContrllers.cs
@@ -1,44 +1,37 @@
 using API.Data;
 using API.Entities;
+using API.Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
-
-// Esto genera https://localhost:5001/api/members le quita el controller
-// porque esta entre corchetes
-// [Route("api/[controller]")]
-// [ApiController]
 [Authorize]
-public class MembersController(AppDbContext context) : BaseApiController
+public class MembersController(IMembersRepository membersRepository) : BaseApiController
 {
-
-    [AllowAnonymous]
-    // Este es el primero en ejecutarse
+    // [AllowAnonymous]
     [HttpGet]
-    // Si se ocupa un IEnumerable es lo mismo que List
-    // List = IEnumerable
-    public async Task<ActionResult<IReadOnlyList<AppUser>>> GetMembers()
+    public async Task<ActionResult<IReadOnlyList<Member>>> GetMembers()
     {
-        var members = await context.Users.ToListAsync();
-        return members;
+
+        return Ok(await membersRepository.GetMembersAsync());
     }
 
-    [AllowAnonymous]
+    // [AllowAnonymous]
     [HttpGet("{id}")] // https://localhost:5001/api/members/bob-id
-    public async Task<ActionResult<AppUser>> GetMember(string id)
+    public async Task<ActionResult<Member>> GetMember(string id)
     {
-        // Toma el id de la URL y muestra la tupla
-        var member = await context.Users.FindAsync(id);
+        var member = await membersRepository.GetMemberAsync(id);
 
-        // Si no encuentra el id manda no encontrado    
         if (member == null) return NotFound();
 
         return member;
     }
-}
 
-// Si lo consultamos desde Postman con el metodo de GEt
-// nos muestra los valores que ya ingresamos
+    [HttpGet("{id}/photos")]
+    public async Task<ActionResult<IReadOnlyList<Photo>>> GetPhoto(string id)
+    {
+        return Ok(await membersRepository.GetPhotosAsync(id));
+    }
+}
